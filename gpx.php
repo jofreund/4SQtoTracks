9fb03abbc283e9b106d81ac8ca7e9a1debc7bfaa
Johannes Freund <johannesfreund@gmx.net>
2012-12-27 15:21:01 +0100

    Adds PHP script and sample GPX

---
+++ gpx.php
@@ -0,0 +1,85 @@
+<?php 
+
+ini_set('error_reporting', E_ALL);
+
+if (file_exists('4sq.gpx')) {
+    $xml = simplexml_load_file('4sq.gpx');
+ 
+    //print_r($xml);
+	$waypoints = $xml->wpt;
+	
+	$finalXML = generateXMLHeader();
+	
+	foreach ($waypoints as $waypoint) {
+//		echo($waypoint['lat']."<br>");
+//		echo($waypoint['lon']."<br>");
+		$square = generateSquare($waypoint['lat'],$waypoint['lon']);
+		
+		$finalXML .= generateTrackFromSquare($square);
+	}
+	
+	$finalXML .= generateXMLFooter();
+	
+	echo($finalXML);
+	
+} else {
+    exit('Failed to open 4sq.gpx.');
+}
+
+function generateSquare($lat,$lon) {
+	$horizontal = floatval(0.000002);
+	$vertical = floatval(0.000000030);
+	
+	$square = array();
+	
+	$lat = floatval($lat);
+	$lon = floatval($lon);
+	
+	$square[0]['lat'] = $lat-$horizontal;
+	$square[0]['lon'] = $lon;
+	
+	$square[1]['lat'] = $lat;
+	$square[1]['lon'] = $lon-$vertical;
+	
+	$square[2]['lat'] = $lat+$horizontal;
+	$square[2]['lon'] = $lon;
+	
+	$square[3]['lat'] = $lat;
+	$square[3]['lon'] = $lon+$vertical;
+	
+	// print_r($square);
+	// 	echo"<br>";
+	
+	return $square;
+}
+
+function generateTrackFromSquare($square) {
+	$xml = "<trk><name>4sq Track</name><trkseg>";
+	foreach ($square as $point) {
+		$xml .= '<trkpt lat="'.$point['lat'].'" lon="'.$point['lon'].'">
+			<ele>0</ele>
+			</trkpt>';
+	}
+	$xml .= "</trkseg></trk>
+	";
+	
+	return $xml;
+	
+}
+
+function generateXMLHeader() {
+	$xml = '
+	<?xml version="1.0" encoding="UTF-8"?>
+	<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:mytracks="http://mytracks.stichling.info/myTracksGPX/1/0" creator="myTracks" version="1.1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">';
+	
+	return $xml;
+}
+
+function generateXMLFooter() {
+	$xml = '</gpx>';
+	
+	return $xml;
+}
+
+
+?>
